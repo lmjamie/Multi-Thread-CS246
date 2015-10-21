f3991f769d4de3beef7f905c1054aec4374e1001
Landon Jamieson <landonjamieson@gmail.com>
2015-10-20 20:49:22 -0600

    Initial commit just created the project as per the instructions.

---
+++ app/src/main/java/edu/byui/landon/multi_thread/OpenFile.java
@@ -0,0 +1,13 @@
+package edu.byui.landon.multi_thread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_file);
+    }
+}
