496c14f3ad7ad09316ac529d4d1d19fc49e5ae2f
Landon Jamieson <landonjamieson@gmail.com>
2015-10-23 23:47:52 -0600

    Created 2 AsyncTasks derived classes that put the loading and creating a file on their own threads. Also added it so it now updates the progress bar. Clear also clears that progress bar. Finished assignment.

--- app/src/main/java/edu/byui/landon/multi_thread/OpenFile.java
+++ app/src/main/java/edu/byui/landon/multi_thread/OpenFile.java
@@ -1,10 +1,12 @@
 package edu.byui.landon.multi_thread;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,45 +25,81 @@ public class OpenFile extends AppCompatActivity {
         setContentView(R.layout.activity_open_file);
     }
 
-    public void createFile(View v) {
-        try (FileWriter fout = new FileWriter(new File(getFilesDir(), "number.txt"))) {
-            for (int i = 1; i <= 10 ; ++i){
-                fout.write(i + System.getProperty("line.separator"));
-                Thread.sleep(250);
+    private class CreateFileTask extends AsyncTask<String, Integer, Void> {
+        @Override
+        protected void onPreExecute() {
+            ((ProgressBar) findViewById(R.id.progressBar)).setProgress(0);
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try (FileWriter fout = new FileWriter(params[0])) {
+                for (int i = 1; i <= 10 ; ++i){
+                    fout.write(i + System.getProperty("line.separator"));
+                    Thread.sleep(250);
+                    publishProgress(i);
+
+                }
             }
+            catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            catch (InterruptedException e) {
+                System.out.println("This won't happen");
+            }
+            return null;
         }
-        catch (IOException e) {
-            e.printStackTrace();
+
+        protected void onProgressUpdate(Integer... progress) {
+            ((ProgressBar) findViewById(R.id.progressBar)).setProgress(10 * progress[0]);
         }
-        catch (InterruptedException e) {
-            System.out.println("This won't happen");
-        }
-        //System.out.println("You clicked Create!");
     }
 
-    public void loadFile(View v) {
-        try (Scanner fin = new Scanner(new File(getFilesDir(), "number.txt"))) {
-            ArrayList<Integer> list = new ArrayList<>();
-            System.out.println(fin.hasNextInt());
-            while (fin.hasNextInt()) {
-                list.add(fin.nextInt());
-                Thread.sleep(250);
+    public void createClick(View v) { new CreateFileTask().execute(getFilesDir() + "/number.txt"); }
+    private class LoadFileTask extends AsyncTask<String, Integer, Void> {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        @Override
+        protected void onPreExecute() { ((ProgressBar) findViewById(R.id.progressBar)).setProgress(0); }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try (Scanner fin = new Scanner(new File(params[0]))) {
+                System.out.println(fin.hasNextInt());
+                while (fin.hasNextInt()) {
+                    list.add(fin.nextInt());
+                    Thread.sleep(250);
+                    publishProgress(10);
+                }
             }
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+            catch (FileNotFoundException e) {
+                System.out.println("Could not find the file number.txt!");
+            }
+            catch (InterruptedException e) {
+                System.out.println("This won't happen");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            ProgressBar pBar = ((ProgressBar) findViewById(R.id.progressBar));
+            pBar.setProgress(pBar.getProgress() + values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            adapter = new ArrayAdapter<>(OpenFile.this, android.R.layout.simple_list_item_1, list);
             ListView myLView = (ListView) findViewById(R.id.listView);
             myLView.setAdapter(adapter);
         }
-        catch (FileNotFoundException e) {
-            System.out.println("Could not find the file number.txt!");
-        }
-        catch (InterruptedException e) {
-            System.out.println("This won't happen");
-        }
-        //System.out.println("You clicked Load!");
-       }
+    }
+    public void loadClick(View v) { new LoadFileTask().execute(getFilesDir() + "/number.txt"); }
 
-    public void clearFile(View v) {
+
+    public void clearClick(View v) {
         adapter.clear();
-        System.out.println("You clicked Clear!");
+        ((ProgressBar) findViewById(R.id.progressBar)).setProgress(0);
     }
 }
